04f457d7777e1da34bfb9eaa0af7cc6d6a3d758e
Ty Le <anhtuan.n.le@gmail.com>
2017-02-05 00:03:54 -0500

    Added csrf validation

--- shopping_cart_app/routes/index.js
+++ shopping_cart_app/routes/index.js
@@ -1,9 +1,14 @@
 var express		= require('express');
 var router 		= express.Router();
 var Product 	= require("../models/product");
+var csrf		= require("csurf");
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
 	Product.find({}, function (err, allProducts){
 		if (err) {
 			console.log(err);
@@ -13,6 +18,15 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+
+router.get("/user/signup", function (req, res, next) {
+	res.render("user/signup", {csrfToken: req.csrfToken()});
+});
+
+
+router.post("/user/signup", function (req, res, next) {
+	res.redirect("/");
+});
 
 
 
