d8a1ff9c668b5ee57867777f4e02699adcde8f2f
Ty Le <anhtuan.n.le@gmail.com>
2017-02-04 23:16:11 -0500

    Seeded data to index page

--- shopping_cart_app/routes/index.js
+++ shopping_cart_app/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express		= require('express');
+var router 		= express.Router();
+var Product 	= require("../models/product");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping App' });
+	Product.find({}, function (err, allProducts){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("shop/index", {allProducts: allProducts})
+		}
+	});
 });
 
+
+
+
 module.exports = router;
+
+
+
+
+// router.get('/', function(req, res, next) {
+// 	Product.find(function(err, docs) {
+// 		res.render('shop/index', { title: 'Shopping App', products: docs});
+// 	});
+// });
