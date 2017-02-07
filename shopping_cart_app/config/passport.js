50f72e06dbec7b6bfd4c925395d85faad4ecfe55
Ty Le <anhtuan.n.le@gmail.com>
2017-02-07 15:46:43 -0500

    Added validation for sign up

--- shopping_cart_app/config/passport.js
+++ shopping_cart_app/config/passport.js
@@ -18,7 +18,17 @@ passport.use("local.signup", new LocalStrategy({
 	passwordField: "password",
 	passReqToCallback: true
 }, function(req, email, password, done) {
-	User.findOne({"email": email}, function(err,user) {
+	req.checkBody("email", "Invalid email").notEmpty().isEmail();
+	req.checkBody("password", "Invalid Password").notEmpty().isLength({min:4});
+	var errors = req.validationErrors();
+	if (errors) {
+		var messages = [];
+		errors.forEach(function(error){
+			messages.push(error.msg);
+		});
+		return done(null, false, req.flash("error", messages));
+	}
+		User.findOne({"email": email}, function(err,user) {
 	if (err) {
 		return done(err);
 	}
