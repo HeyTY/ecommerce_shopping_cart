069bad20489c42dd73bfdd3d66e00ab5f9662dd4
Ty Le <anhtuan.n.le@gmail.com>
2017-02-08 22:17:49 -0500

    Added cart model and logic

--- shopping_cart_app/app.js
+++ shopping_cart_app/app.js
@@ -10,6 +10,7 @@ var session			= require('express-session');
 var passport		= require('passport');
 var flash			= require('connect-flash'); 
 var validator		= require('express-validator');
+var MongoStore		= require('connect-mongo')(session);
 
 var index 			= require('./routes/index');
 var userRoutes		= require('./routes/user');
@@ -33,7 +34,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: "link-nsx", resave: false, saveUninitialized: false}));
+app.use(session({
+	secret: "link-nsx", 
+	resave: false, 
+	saveUninitialized: false,
+	store: new MongoStore({ mongooseConnection: mongoose.connection}),
+			// Set at 180 mins
+	cookie: { maxAge: 180 * 60 * 1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
 	res.locals.login = req.isAuthenticated();
+	res.locals.session = req.session;
 	next();
 });
 
